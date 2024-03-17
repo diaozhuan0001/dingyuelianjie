6a3fc23f56448918b62bfba61a941bc6e8915899
diaozhuan0001 <66158986+diaozhuan0001@users.noreply.github.com>
2024-03-17 16:56:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'ea844215-5cbb-4529-a1c1-d52842aa6cbc';
+let userID = '542d4400-2ff5-479e-b2f5-c273d7bbc535';
 
 let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'
 
